623e4d03f44f18b1f321e013316d7fdc29956809
bulochkka <ram23zan@mail.ru>
2024-08-05 15:06:13 +0300

    ugu

--- PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Needle7.java
+++ PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Needle7.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
     private Deth8 deth;
-    
+
     @Autowired
-    public void Needle7(Deth8 deth) {
+    public void setDeth(Deth8 deth) {
         this.deth = deth;
     }
-    
+
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
